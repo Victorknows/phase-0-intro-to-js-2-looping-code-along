7f9eb8d09e6416473f016bebff1596a4dbdd8800
Victor Muhoro <victormuhoro7@gmail.com>
2023-12-18 00:34:09 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//Write code here
+function writeCards (par1, par2) {
+    const messages =[];
+    
+    for (let i = 0; i < par1.length; i++) {
+      
+        messages.push(`Thank you, ${par1[i]}, for the wonderful surprise gift!`)
+
+    }
+  
+    return messages;
+}
+
+
+function countDown(myNum){
+    while (myNum >= 0){
+        console.log(myNum--)
+    }
+
+}
+
+countDown(10)
